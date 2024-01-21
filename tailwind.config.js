d8d32db0bb26ca5935e9decf90f0885951707456
Muhammad-Hafiz-Fadhilah <mhafizfadilah10@gmail.com>
2024-01-21 11:09:30 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+  module.exports = {
+    theme: {
+       extend: {
+          fontFamily: {
+             'poppins': ['Poppins'],
+          },
+          colors: {
+            gray: "#e5e5e5"
+          }
+       }
+    }
+  }
